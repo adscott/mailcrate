2f202e948e29823e1dfc24911b545a0a6e6cbc51
Adam Scott <adam.d.scott@gmail.com>
2012-02-12 14:50:11 +0800

    Basic SMTP conversation.

--- lib/mailbox.rb
+++ lib/mailbox.rb
@@ -1,17 +1,88 @@
 require 'socket'
 
 class Mailbox
+
+	attr_reader :mails
+
 	def initialize(port)
 		@port = port
+		@mails = []
 	end
 
 
 	def start
 		@service = TCPServer.new('localhost', @port )
+		Thread.new { accept(@service) }
 	end
 
 	def stop
 		@service.close unless @service.nil? || @service.closed? 
 	end
+
+	def accept( service )
+    while session = service.accept
+      
+      class << session
+        def get_line
+          line = gets
+          line.chomp! unless line.nil?
+          line          
+        end
+      end
+      
+      begin
+        serve( session )
+      rescue Exception => e
+        puts e.message        
+      end
+    end    
+  end
+
+  def serve( connection )
+  	connection.puts( "220 localhost mailbox ready ESTMP" )
+    helo = connection.get_line
+    
+    if helo =~ /^EHLO\s+/
+      connection.puts "250-localhost mailbox here"
+      connection.puts "250 HELP"
+    end
+
+    from = connection.get_line
+    connection.puts( "250 ok" )
+
+    
+    to_list = []
+    
+    loop do
+      to = connection.get_line
+      break if to.nil?
+
+      if to =~ /^DATA/
+        connection.puts( "354 start" )
+        break
+      else
+        to_list << to
+        connection.puts( "250 ok" )
+      end
+    end
+    
+    lines = []
+    loop do
+      line = connection.get_line
+      break if line.nil? || line == "."
+      lines << line
+    end
+
+    connection.puts "250 ok"
+    connection.gets
+    connection.puts "221 bye"
+    connection.close
+
+    @mails << { 
+	    :from => from.gsub(/MAIL FROM:\s*/, ''),
+	    # :to_list => to_list.map { |to| to.gsub( /RCPT TO:\s*/, "" ) }
+	    # :body => lines.join( "\n" ) 
+	  }
+  end
 
 end
--- spec/mailbox_spec.rb
+++ spec/mailbox_spec.rb
@@ -11,21 +11,49 @@ describe Mailbox do
 		@mailbox.stop
 	end
 
-	it 'should open a port' do
-		@mailbox.start
+	describe 'ports' do
 
-		port(2525).should be_open
-	end
+		it 'should open a port' do
+			@mailbox.start
 
-	it 'should close a port' do
-		@mailbox.start
-		@mailbox.stop
+			port(2525).should be_open
+		end
 
-		port(2525).should_not be_open
+		it 'should close a port' do
+			@mailbox.start
+			@mailbox.stop
+
+			port(2525).should_not be_open
+	  end
+
+	  it 'should not allow a second server to be started on the same port'
+
+    def port(port)
+		  Port.new(port)
+		end
   end
 
-  def port(port)
-	  Port.new(port)
-	end
+  describe 'should accept SMTP traffic' do
+
+  	it 'should recieve email' do
+  		@mailbox.start
+
+  		socket = TCPSocket.open('localhost', 2525)
+			socket.puts('helo localhost.localdomain')
+			socket.puts('MAIL FROM:myaddress@mydomain.com')
+			socket.puts('RCPT TO:somone@somedomain.com')
+			socket.puts('DATA')
+			socket.puts('Hello Fred, can you call me?')
+			socket.puts('.')
+			socket.puts('QUIT')
+
+			while(@mailbox.mails.empty?) do
+				sleep(0.1)
+			end
+
+			@mailbox.mails[0][:from].should == 'myaddress@mydomain.com'
+  	end
+
+  end
 	
 end
