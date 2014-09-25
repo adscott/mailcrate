cdcdabc41d4fefe272e09b5f4c52a0b58aada418
grosser <michael@grosser.it>
2014-09-24 23:21:02 -0700

    fix or pending test

--- spec/mailcrate_spec.rb
+++ spec/mailcrate_spec.rb
@@ -6,52 +6,54 @@ require 'matchers/eventually'
 TEST_PORT = 8025
 
 describe Mailcrate do
+  let!(:mailcrate) { Mailcrate.new(TEST_PORT) }
 
-  before(:each) do
-    @mailcrate = Mailcrate.new(TEST_PORT)
-  end
-
-  after(:each) do
-    @mailcrate.stop
-  end
+  after { mailcrate.stop }
 
   describe 'ports' do
-
-    it 'should open a port' do
-      @mailcrate.start
+    it 'opens a port' do
+      mailcrate.start
 
       expect(port(TEST_PORT)).to be_open
     end
 
-    it 'should close a port' do
-      @mailcrate.start
-      @mailcrate.stop
+    it 'can read the port' do
+      mailcrate.start # makes .stop not blow up
+
+      expect(mailcrate.port).to eq TEST_PORT
+    end
+
+    it 'closes a port' do
+      mailcrate.start
+      mailcrate.stop
 
       expect(port(TEST_PORT)).to_not be_open
     end
 
-    it 'should not allow a second server to be started on the same port'
+    it 'does not allow a second server to be started on the same port' do
+      mailcrate.start
+      expect {
+        Mailcrate.new(TEST_PORT).start
+      }.to raise_error(Errno::EADDRINUSE)
+    end
   end
 
   describe 'starting and stopping' do
-
     it 'should block when start is called until SMTP server is ready' do
       slow_server = SlowTCPServer.new('localhost', TEST_PORT)
 
-      @mailcrate.start(:service => slow_server)
+      mailcrate.start(:service => slow_server)
 
       socket = TCPSocket.open('localhost', TEST_PORT)
       welcome_message = socket.gets.chomp
 
       expect(welcome_message).to eq '220 localhost mailcrate ready ESTMP'
     end
-
   end
 
   describe 'should accept SMTP traffic' do
-
     it 'should recieve email' do
-      @mailcrate.start
+      mailcrate.start
 
       socket = TCPSocket.open('localhost', TEST_PORT)
       socket.puts('helo localhost.localdomain')
@@ -62,14 +64,12 @@ describe Mailcrate do
       socket.puts('.')
       socket.puts('QUIT')
 
-      expect(@mailcrate.mails).to eventually have(1).items
-      expect(@mailcrate.mails[0][:from]).to eq 'myaddress@mydomain.com'
-      expect(@mailcrate.mails[0][:to_list]).to include 'somone@somedomain.com'
-      expect(@mailcrate.mails[0][:body]).to eq 'Hello Fred, can you call me?'
+      expect(mailcrate.mails).to eventually have(1).items
+      expect(mailcrate.mails[0][:from]).to eq 'myaddress@mydomain.com'
+      expect(mailcrate.mails[0][:to_list]).to include 'somone@somedomain.com'
+      expect(mailcrate.mails[0][:body]).to eq 'Hello Fred, can you call me?'
     end
-
   end
-
 end
 
 class SlowTCPServer < TCPServer
